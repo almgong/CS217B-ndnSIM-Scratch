96308f20c611b5ad467a4e0bcfb711e4eda516e3
Allen G <alm.gong@gmail.com>
2016-05-26 13:17:58 -0700

    create a stub for consumers, we are ready to go

---
+++ helpers/consumer-PRT.hpp
@@ -0,0 +1,17 @@
+#ifndef CONSUMER_PRT_H_
+#define CONSUMER_PRT_H_
+
+#include "ns3/ndnSIM/apps/ndn-app.hpp"
+
+/**
+ * Represents a consumer under the PRT scheme (ideally sends a report or something
+ * like that on receipt of invalid data).
+**/
+
+namespace ns3 {
+
+	class ConsumerPRT : public ndn::App {
+
+	};
+
+}
--- ndn-vanilla-prt-app.cc
+++ ndn-vanilla-prt-app.cc
@@ -2,7 +2,15 @@
 #include "ns3/network-module.h"
 #include "ns3/ndnSIM-module.h"
 #include <iostream>
+
+
 namespace ns3 {
+
+	/**
+	* Driver program, for vanilla PRT logic
+	* Note we need to implement logic of consumer and producer types
+	* (e.g., regular consumers, "bad producers") before running this.
+	**/
 	int main(int argc, char**argv) {
 		std::cout << "hello\n";
 		return 0;
