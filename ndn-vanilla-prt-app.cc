d9ba8948dd3ad0102087596f914d956ac070f44b
Allen G <alm.gong@gmail.com>
2016-05-26 12:58:35 -0700

    create app stub for vanilla prt scenario

---
+++ ndn-vanilla-prt-app.cc
@@ -0,0 +1,16 @@
+#include "ns3/core-module.h"
+#include "nds/network-module.h"
+#include "ns3/ndnSIM-moduke.h"
+
+namespace ns3 {
+	int main(int argc, char**argv) {
+		std::cout << "hello" << end;
+	}
+
+	return 0;
+}
+
+int
+main(int argc, char** argv) {
+	return ns3::main(argc, argv);
+}
